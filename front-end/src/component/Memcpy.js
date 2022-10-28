014a820c1d4cda090bb68ca506a62cc630816c79
Yuewen <yuewenkapa@gmail.com>
2022-10-27 23:56:46 -0400

    Change app flow and group info page - Xi Liu

---
+++ front-end/src/component/Memcpy.js
@@ -0,0 +1,8 @@
+function Memcpy(input) {
+    let copy = document.getElementById(input);
+    copy.select();
+    navigator.clipboard.writeText(copy.value);
+    alert('Link copied: ' + copy.value);
+}
+
+export default Memcpy;
