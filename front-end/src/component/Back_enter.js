f62b3610157b8e2a09fda385c6edfcf0ce175256
Xi Liu <xi1331@126.com>
2022-11-08 09:56:57 -0500

    update group creation express

--- front-end/src/component/Back_enter.js
+++ front-end/src/component/Back_enter.js
@@ -6,7 +6,7 @@ function Back_enter(s)
     return(
         <div>
             <Button str_array = {[{str: 'Back', left: 1, cur_id: s.cur_id}]} type = {11}></Button>
-            <Button str_array = {[{str: 'Create', left: 0, cur_id: s.cur_id}]} type = {11}></Button>
+            <Button str_array = {[{str: 'Enter', left: 0, cur_id: s.cur_id}]} type = {11}></Button>
         </div>
     );
 }
