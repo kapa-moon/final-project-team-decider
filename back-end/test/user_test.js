2d45f02b1cb786d478326a5b74a712a4f0dc1335
Yuewen <yuewenkapa@gmail.com>
2022-11-11 22:24:22 -0500

    add user tests

---
+++ back-end/test/user_test.js
@@ -0,0 +1,108 @@
+let app = require('../app'),
+    express = require('express'),
+    assert = require('assert'),
+    request = require('supertest'),
+    chai = require('chai'),
+    { expect } = require('chai'),
+    chai_http = require('chai-http'),
+    should = chai.should();
+chai.use(chai_http);
+
+describe('user test', function () {
+    describe('get /user', function () {
+        it('should have 200 success status response, res.body should be an array', function (done) {
+            chai.
+                request(app)
+                .get('/user')
+                .end(function (e, res) {
+                    if (e)
+                        throw e;
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('array');
+                    done();
+                });
+        })
+    });
+
+    describe('', function () {
+        let user_id = Math.random().toString().slice(2);
+        let email = `${Math.random().toString().slice(2)}@gmail.com`;
+        let password = Math.random().toString().slice(2);
+        let name = `${Math.random().toString().slice(2)}name`;
+        let my_location = `${Math.random().toString().slice(2)}location`;
+        let my_groups = [Math.random().toString().slice(2), Math.random().toString().slice(2)];
+        let current_group = Math.random().toString().slice(2);
+        let voted_locations = [Math.random().toString().slice(2), Math.random().toString().slice(2)];
+        describe('post /user/add', function () {
+            it('should have 200 success status response, res.body should be a string', function (done) {
+                chai.
+                    request(app)
+                    .post('/user/add')
+                    .send({
+                        user_id: user_id,
+                        email: email,
+                        name: name,
+                        password: password,
+                        my_location: my_location,
+                        my_groups: my_groups,
+                        current_group: current_group,
+                        voted_locations: voted_locations,
+                    })
+                    .end(function (e, res) {
+                        if (e)
+                            throw e;
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.be.an('string');
+                        done();
+                    });
+            })
+        });
+
+        describe('get /user/vote', function () {
+            let location_id = Math.random().toString().slice(2);
+            let user_id = Math.random().toString().slice(2);
+            it('should have 200 success status response, res.body should be a user string', function (done) {
+                chai.
+                    request(app)
+                    .get('/user/vote')
+                    .send({
+                        user_id: user_id,
+                        location_id: location_id,
+                    })
+                    .end(function (e, res) {
+                        if (e)
+                            throw e;
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.be.an('array');
+                        done();
+                    });
+            })
+        });
+
+        describe('get /user/unvote', function () {
+            let location_id = Math.random().toString().slice(2);
+            let user_id = Math.random().toString().slice(2);
+            it('should have 200 success status response, res.body should be a user string', function (done) {
+                chai.
+                    request(app)
+                    .get('/user/unvote')
+                    .send({
+                        user_id: user_id,
+                        location_id: location_id,
+                    })
+                    .end(function (e, res) {
+                        if (e)
+                            throw e;
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.be.an('array');
+                        done();
+                    });
+            })
+        });
+
+
+
+
+    });
+
+});
