211e007e473fc22c3eb1ff2415f62fa06efa300f
Yuewen <yuewenkapa@gmail.com>
2022-11-13 02:32:24 -0500

    Update selector style

--- front-end/src/component/Selector.js
+++ front-end/src/component/Selector.js
@@ -12,11 +12,11 @@ function Selector() {
                 </div>
             </Link>
             <SearchBar></SearchBar>
-            <div className='selectors flex justify-between h-20'>
+            <div className='selectors flex justify-between h-10'>
                 <Link to='/search' className='text-xl'>Search</Link>
                 <Link to='/recommend' className='text-xl '>Recommend</Link>
                 <Link to='/group' className='text-xl '>Group List</Link>
-                <Link to='/go' className='text-xl '>Go!</Link>
+                <Link to='/go' className='text-xl underline decoration-border-pink decoration-solid underline-offset-4 text-sharp-pink'>Go!✌️</Link>
             </div>
         </div>
     );
