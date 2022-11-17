07af26cc9f2f55b935ee48278753e9c97d24c14e
Xi Liu <xi1331@126.com>
2022-11-17 18:08:13 -0500

    update

--- front-end/src/component/Signup.js
+++ front-end/src/component/Signup.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, {useRef} from 'react';
 import '../App.css';
 import Button from './Button';
 import {useNavigate} from "react-router-dom";
