05801a80b3421fd05e863fea064b4daa6fb95fe4
Xi Liu <xi1331@126.com>
2022-11-30 21:44:26 -0500

    Update SearchLocation.js

--- front-end/src/component/SearchLocation.js
+++ front-end/src/component/SearchLocation.js
@@ -6,7 +6,8 @@ function SearchLocation()
 
     const [myCurLocation, setMyCurLocation] = useState(() => {
        const curLocation = window.localStorage.getItem('myCurLocation');
-       return curLocation === undefined ?  {lat: -73.996925, lng: 40.729675} : JSON.parse(curLocation);
+       //  return null;
+       return curLocation===undefined ? {lat: -73.996925, lng: 40.729675}: JSON.parse(curLocation);
     });
 
     // const [myCurLocation, setMyCurLocation] = useState(
